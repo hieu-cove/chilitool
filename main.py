7e2d88edfcab6c83a2bf537e35b9f16b0df04c86
Hieu Nguyen <hieu.nguyen@covetool.com>
2022-10-13 13:38:56 -0400

    feat: add bootstrap index

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
+
+
+app.mount("/", StaticFiles(directory="static"), name="static")
+
 
 class Msg(BaseModel):
     msg: str
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
 @app.get("/path")
 async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+    return {
+        "message": "This is /path endpoint, use a post request to transform the text to uppercase"
+    }
 
 
 @app.post("/path")
@@ -24,4 +26,6 @@ async def demo_post(inp: Msg):
 
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    return {
+        "message": f"This is /path/{path_id} endpoint, use post request to retrieve result"
+    }
