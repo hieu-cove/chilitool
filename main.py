fe83da44db8b2ad8dd59920b5676c9c16a90d3f3
Hieu Nguyen <hieu.nguyen@covetool.com>
2022-10-13 15:41:36 -0400

    feat: add backend for voting

--- main.py
+++ main.py
@@ -1,31 +1,69 @@
-from fastapi import FastAPI
+import http
+from typing import List
+
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from models import Constituent, Vote, Voter, engine
 
 app = FastAPI()
 
 
-app.mount("/", StaticFiles(directory="static"), name="static")
+class Rank(BaseModel):
+    constituent_id: int
+    rank: int
 
 
-class Msg(BaseModel):
-    msg: str
+class VoteRequest(BaseModel):
+    voter_name: str
+    rankings: List[Rank]
 
 
-@app.get("/path")
-async def demo_get():
-    return {
-        "message": "This is /path endpoint, use a post request to transform the text to uppercase"
-    }
+@app.put("/vote")
+async def put_vote(req: VoteRequest):
+    session = Session(engine)
+    voter = session.scalars(
+        select(Voter).filter_by(name=req.voter_name).limit(1)
+    ).one_or_none()
+    if not voter:
+        voter = Voter(name=req.voter_name)
+        session.add(voter)
+        session.commit()
+    rankings = {}
+    check_rankings = {}
+    for rank in req.rankings:
+        if rank.rank in check_rankings:
+            return HTTPException(
+                status_code=http.HTTPStatus.BAD_REQUEST,
+                detail=f"Ranked both {rank.constituent_id} "
+                f"and {check_rankings[rank.rank]} number {rank.rank}",
+            )
+        check_rankings[rank.rank] = rank.constituent_id
+        rankings[rank.constituent_id] = rank.rank
+    constituents = session.scalars(
+        select(Constituent).filter(Constituent.id.in_(list(rankings.keys())))
+    )
+    for constituent in constituents:
+        session.add(
+            Vote(
+                voter_id=voter.id,
+                contituent_id=constituent.id,
+                rank=rankings[constituent.id],
+            )
+        )
+    session.commit()
+    return rankings
 
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
+@app.get("/constituents")
+async def get_constituents():
+    session = Session(engine)
+    filter = select(Constituent)
+    constituents = session.scalars(filter)
+    return list(map(lambda c: c.to_json(), constituents))
 
 
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {
-        "message": f"This is /path/{path_id} endpoint, use post request to retrieve result"
-    }
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
---
+++ models.py
@@ -0,0 +1,47 @@
+from sqlalchemy import ForeignKey, Integer, String, UniqueConstraint, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Constituent(Base):
+    __tablename__ = "constituent"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(), unique=True)
+
+    def __repr__(self) -> str:
+        return f"Constituent(id={self.id!r}, name={self.name!r})"
+
+    def to_json(self) -> dict:
+        return {"id": self.id, "name": self.name}
+
+
+class Voter(Base):
+    __tablename__ = "voter"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(), unique=True)
+
+    def __repr__(self) -> str:
+        return f"Voter(id={self.id!r}, name={self.name!r})"
+
+    def to_json(self) -> dict:
+        return {"id": self.id, "name": self.name}
+
+
+class Vote(Base):
+    __tablename__ = "vote"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    voter_id: Mapped[int] = mapped_column(ForeignKey("voter.id"))
+    constituent_id: Mapped[int] = mapped_column(ForeignKey("constituent.id"))
+    rank: Mapped[int] = mapped_column(Integer())
+    UniqueConstraint(voter_id, rank)
+
+
+# Connect to your postgres DB
+engine = create_engine(
+    "postgresql://postgres:FQQXJyd3c2YkIu2XB0Nl@"
+    "containers-us-west-17.railway.app:6799/railway",
+    echo=True,
+)
